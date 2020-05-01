6d69ae825529daae911432542bff405d203b2089
Darren Tebo <darrentebo83@outlook.com>
2020-04-30 20:46:31 -0400

    passed fourty six tests

--- index.js
+++ index.js
@@ -180,8 +180,26 @@ class Instructor extends Lambdasian{
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian {
+  constructor(stObj){
+    super(stObj);
+    this.name = stObj.name;
+    this.previousBackground = stObj.previousBackground;
+    this.className = stObj.className;
+    this.favSubjects = stObj.favSubjects;
+  }
 
+  listSubjects(){
+    return `Loving ${this.favSubjects.toString()}!`;
+  }
+
+  PRAssignment(subject){
+    return `${this.name} has submitted a PR for ${subject}`;
+  }
+
+  sprintChallenge(subject){
+    return `${this.name} has begun a sprint challenge on ${subject}`;
+  }
 }
 
 /*
