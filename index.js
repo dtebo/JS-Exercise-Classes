ed20a6df2487959bdb5643343d1fe6f9764eaa8b
Darren Tebo <darrentebo83@outlook.com>
2020-04-30 20:31:56 -0400

    passed thirty five tests

--- index.js
+++ index.js
@@ -148,8 +148,21 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+class Instructor extends Lambdasian{
+  constructor(instObj){
+    super(instObj);
+    this.specialty = instObj.specialty;
+    this.favLanguage = instObj.favLanguage;
+    this.catchPhrase = instObj.catchPhrase;
+  }
 
+  demo(subject){
+    return `Today we are learning about ${subject}`;
+  }
+
+  grade(student, subject){
+    return `${student.name} receives a perfect score on ${subject}`;
+  }
 }
 
 /*
